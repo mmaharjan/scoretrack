1b3f926d595f52d57073f75e9846fbffcf3a5807
Minesh Maharjan <minesh.maharjan88@gmail.com>
2019-06-18 17:25:36 -0700

    ResourceServer initial draft complete

--- src/main/java/com/webservices/scoretrack/auth/ScoretrackResourceServerConfig.java
+++ src/main/java/com/webservices/scoretrack/auth/ScoretrackResourceServerConfig.java
@@ -1,11 +1,27 @@
 package com.webservices.scoretrack.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AuthenticationEntryPoint;
+import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.util.Map;
 
 @Configuration
 @EnableResourceServer
@@ -14,6 +30,9 @@ public class ScoretrackResourceServerConfig extends ResourceServerConfigurerAdap
 
     static final String RESOURCE_ID = "scoretrack_resource";
 
+    @Autowired
+    CustomClientDetailsService customClientDetailsService;
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId(RESOURCE_ID);
@@ -21,4 +40,61 @@ public class ScoretrackResourceServerConfig extends ResourceServerConfigurerAdap
         //accessDeniedHandler(customAccessDeniedHandler()).authenticationEntryPoint(customAuthenticationEntryPoint())
     }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+                .antMatchers(("/api/games/talk")).permitAll()
+                .antMatchers("/v1/account/**").hasAnyRole("USER", "SYSTEM_ADMIN", "GROUP_ADMIN")
+                .antMatchers("v1/user/game").hasAnyRole("USER", "SYSTEM_ADMIN");
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setClientDetailsService(customClientDetailsService);
+        return tokenServices;
+    }
+
+
+    @Bean
+    @Primary
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+
+    @Bean
+    protected JwtAccessTokenConverter accessTokenConverter() {
+        String jwtKeyStorePassword = "";
+        String oauth2JwtKeyStoreKeyPairAlias = "";
+        String oauth2JwtKeyStoreInternal = "eag-jwt-keystore.jks";
+
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(oauth2JwtKeyStoreInternal),
+                jwtKeyStorePassword.toCharArray());
+
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        // for asymmetric signing/verification
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(oauth2JwtKeyStoreKeyPairAlias));
+        return converter;
+    }
+
+
+    /*@Bean
+    public AuthenticationEntryPoint customAuthenticationEntryPoint() {
+        final OAuth2AuthenticationEntryPoint entryPoint = new OAuth2AuthenticationEntryPoint();
+        entryPoint.setExceptionTranslator(customExceptionTranslator());
+        return entryPoint;
+    }*/
+
+    /*@Bean
+    public WebResponseExceptionTranslator<Map> customExceptionTranslator() {
+        return new CustomWebResponseExceptionTranslator();
+    }*/
 }
---
+++ src/main/java/com/webservices/scoretrack/auth/ScoretrackWebServerConfig.java
@@ -0,0 +1,54 @@
+package com.webservices.scoretrack.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(1) // need to make the Basic http authentication to run before the OAuth2 resource server authentication
+public class ScoretrackWebServerConfig extends WebSecurityConfigurerAdapter {
+
+    /** uncomment this method later on in local dev
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        if(!profile is dev) {
+            web.ignoring().anyRequest();
+        }
+    }
+    */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/api/admin/**")
+                .authorizeRequests()
+                //ROLE will automatically be prepended
+                .anyRequest().hasAnyRole("USER", "SYSTEM_ADMIN", "GROUP_ADMIN" );
+
+        http
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("demo1")
+                .password("demo1Pass")
+                .roles("SYSTEM_ADMIN");
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("demo2")
+                .password("demo2Pass")
+                .roles("USER");
+    }
+}
